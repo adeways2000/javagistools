9b35c13dc0a39d00b11853d40ec91f405b4ea728
Adebisi Ayokunle <adeways2000@gmail.com>
2024-07-20 12:03:09 +0200

    Add files via upload

---
+++ javagistool/src/main/java/org/geotools/App.java
@@ -0,0 +1,23 @@
+package org.geotools;
+
+import org.geotools.fileloader.QueryLab;
+import org.geotools.fileloader.ShpFileLoad;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) throws Exception {
+
+
+        JFrame frame = new QueryLab();
+        frame.setVisible(true);
+
+
+
+        //ShpFileLoad.shpFileLoader();
+    }
+}
